672d9830d454f66f53377884f8a6ac1807d87f6c
fitz <wenjingbiao@outlook.com>
2025-07-17 11:44:54 +0800

    feat: 🎸 Migrate to PostCSS 8

--- README_CN.md
+++ README_CN.md
@@ -1,7 +1,10 @@
 # postcss-px-to-viewport
+
 [![NPM version](https://badge.fury.io/js/postcss-px-to-viewport.svg)](http://badge.fury.io/js/postcss-px-to-viewport)
 
 [English](README.md) | 中文
+
+#### 此 Fork是为了迁移到 postcss8
 
 将px单位转换为视口单位的 (vw, vh, vmin, vmax) 的 [PostCSS](https://github.com/postcss/postcss) 插件.
 
@@ -13,7 +16,7 @@
 
 ```css
 .class {
-  margin: -10px .5vh;
+  margin: -10px 0.5vh;
   padding: 5vmin 9.5px 1px;
   border: 3px solid black;
   border-bottom-width: 1px;
@@ -41,9 +44,10 @@
 ```
 
 ### 输出
+
 ```css
 .class {
-  margin: -3.125vw .5vh;
+  margin: -3.125vw 0.5vh;
   padding: 5vmin 2.96875vw 1px;
   border: 0.9375vw solid black;
   border-bottom-width: 1px;
@@ -72,11 +76,15 @@
 ## 上手
 
 ### 安装
+
 使用npm安装
+
 ```
 $ npm install postcss-px-to-viewport --save-dev
 ```
+
 或者使用yarn进行安装
+
 ```
 $ yarn add -D postcss-px-to-viewport
 ```
@@ -84,6 +92,7 @@ $ yarn add -D postcss-px-to-viewport
 ### 配置参数
 
 默认参数:
+
 ```js
 {
   unitToConvert: 'px',
@@ -103,32 +112,33 @@ $ yarn add -D postcss-px-to-viewport
   landscapeWidth: 568
 }
 ```
+
 - `unitToConvert` (String) 需要转换的单位，默认为"px"
 - `viewportWidth` (Number) 设计稿的视口宽度
 - `unitPrecision` (Number) 单位转换后保留的精度
 - `propList` (Array) 能转化为vw的属性列表
   - 传入特定的CSS属性；
-  - 可以传入通配符"*"去匹配所有属性，例如：['*']；
-  - 在属性的前或后添加"*",可以匹配特定的属性. (例如['*position*'] 会匹配 background-position-y)
+  - 可以传入通配符"_"去匹配所有属性，例如：['_']；
+  - 在属性的前或后添加"*",可以匹配特定的属性. (例如['*position\*'] 会匹配 background-position-y)
   - 在特定属性前加 "!"，将不转换该属性的单位 . 例如: ['*', '!letter-spacing']，将不转换letter-spacing
-  - "!" 和 "*"可以组合使用， 例如: ['*', '!font*']，将不转换font-size以及font-weight等属性
+  - "!" 和 "_"可以组合使用， 例如: ['_', '!font\*']，将不转换font-size以及font-weight等属性
 - `viewportUnit` (String) 希望使用的视口单位
 - `fontViewportUnit` (String) 字体使用的视口单位
 - `selectorBlackList` (Array) 需要忽略的CSS选择器，不会转为视口单位，使用原有的px等单位。
-    - 如果传入的值为字符串的话，只要选择器中含有传入值就会被匹配
-        - 例如 `selectorBlackList` 为 `['body']` 的话， 那么 `.body-class` 就会被忽略
-    - 如果传入的值为正则表达式的话，那么就会依据CSS选择器是否匹配该正则
-        - 例如 `selectorBlackList` 为 `[/^body$/]` , 那么 `body` 会被忽略，而 `.body` 不会
+  - 如果传入的值为字符串的话，只要选择器中含有传入值就会被匹配
+    - 例如 `selectorBlackList` 为 `['body']` 的话， 那么 `.body-class` 就会被忽略
+  - 如果传入的值为正则表达式的话，那么就会依据CSS选择器是否匹配该正则
+    - 例如 `selectorBlackList` 为 `[/^body$/]` , 那么 `body` 会被忽略，而 `.body` 不会
 - `minPixelValue` (Number) 设置最小的转换数值，如果为1的话，只有大于1的值会被转换
 - `mediaQuery` (Boolean) 媒体查询里的单位是否需要转换单位
 - `replace` (Boolean) 是否直接更换属性值，而不添加备用属性
 - `exclude` (Array or Regexp) 忽略某些文件夹下的文件或特定文件，例如 'node_modules' 下的文件
-    - 如果值是一个正则表达式，那么匹配这个正则的文件会被忽略
-    - 如果传入的值是一个数组，那么数组里的值必须为正则
+  - 如果值是一个正则表达式，那么匹配这个正则的文件会被忽略
+  - 如果传入的值是一个数组，那么数组里的值必须为正则
 - `include` (Array or Regexp) 如果设置了`include`，那将只有匹配到的文件才会被转换，例如只转换 'src/mobile' 下的文件
-    (`include: /\/src\/mobile\//`)
-    - 如果值是一个正则表达式，将包含匹配的文件，否则将排除该文件
-    - 如果传入的值是一个数组，那么数组里的值必须为正则
+  (`include: /\/src\/mobile\//`)
+  - 如果值是一个正则表达式，将包含匹配的文件，否则将排除该文件
+  - 如果传入的值是一个数组，那么数组里的值必须为正则
 - `landscape` (Boolean) 是否添加根据 `landscapeWidth` 生成的媒体查询条件 `@media (orientation: landscape)`
 - `landscapeUnit` (String) 横屏时使用的单位
 - `landscapeWidth` (Number) 横屏时使用的视口宽度
@@ -138,10 +148,12 @@ $ yarn add -D postcss-px-to-viewport
 #### Ignoring (需要翻译帮助。)
 
 You can use special comments for ignore conversion of single lines:
+
 - `/* px-to-viewport-ignore-next */` — on a separate line, prevents conversion on the next line.
 - `/* px-to-viewport-ignore */` — after the property on the right, prevents conversion on the same line.
 
 Example:
+
 ```css
 /* example input: */
 .class {
@@ -167,37 +179,39 @@ There are several more reasons why your pixels may not convert, the following op
 #### 使用PostCss配置文件时
 
 在`postcss.config.js`添加如下配置
+
 ```js
 module.exports = {
   plugins: {
     // ...
-    'postcss-px-to-viewport': {
+    "postcss-px-to-viewport": {
       // options
-    }
-  }
-}
+    },
+  },
+};
 ```
 
 #### 直接在gulp中使用，添加gulp-postcss
 
 在 `gulpfile.js` 添加如下配置:
+
 ```js
-var gulp = require('gulp');
-var postcss = require('gulp-postcss');
-var pxtoviewport = require('postcss-px-to-viewport');
+var gulp = require("gulp");
+var postcss = require("gulp-postcss");
+var pxtoviewport = require("postcss-px-to-viewport");
 
-gulp.task('css', function () {
+gulp.task("css", function () {
+  var processors = [
+    pxtoviewport({
+      viewportWidth: 320,
+      viewportUnit: "vmin",
+    }),
+  ];
 
-    var processors = [
-        pxtoviewport({
-            viewportWidth: 320,
-            viewportUnit: 'vmin'
-        })
-    ];
-
-    return gulp.src(['build/css/**/*.css'])
-        .pipe(postcss(processors))
-        .pipe(gulp.dest('build/css'));
+  return gulp
+    .src(["build/css/**/*.css"])
+    .pipe(postcss(processors))
+    .pipe(gulp.dest("build/css"));
 });
 ```
 
@@ -209,10 +223,13 @@ gulp.task('css', function () {
 ## 测试
 
 为了跑测试案例，您需要安装开发套件:
+
 ```
 $ npm install
 ```
+
 然后输入下面的命令:
+
 ```
 $ npm run test
 ```
@@ -227,8 +244,8 @@ $ npm run test
 
 ## 作者
 
-* [Dmitry Karpunin](https://github.com/KODerFunk) - *Initial work*
-* [Ivan Bunin](https://github.com/chernobelenkiy)
+- [Dmitry Karpunin](https://github.com/KODerFunk) - _Initial work_
+- [Ivan Bunin](https://github.com/chernobelenkiy)
 
 在 [contributors](https://github.com/evrone/postcss-px-to-viewport/contributors) 里可以看到谁参与了本项目.
 
@@ -247,4 +264,4 @@ $ npm run test
 
 ## 借鉴自
 
-* 受 https://github.com/cuth/postcss-pxtorem/ 启发有了这个项目
+- 受 https://github.com/cuth/postcss-pxtorem/ 启发有了这个项目
--- index.js
+++ index.js
@@ -1,82 +1,131 @@
-'use strict';
+// Not anything inside double quotes
+// Not anything inside single quotes
+// Not anything inside url()
+// Any digit followed by px
+// !singlequotes|!doublequotes|!url()|pixelunit
+function getUnitRegexp(unit) {
+  return new RegExp(
+    "\"[^\"]+\"|'[^']+'|url\\([^\\)]+\\)|(\\d*\\.?\\d+)" + unit,
+    "g"
+  );
+}
+export const filterPropList = {
+  exact: (list) => list.filter((m) => m.match(/^[^\*\!]+$/)),
+  contain: (list) =>
+    list
+      .filter((m) => m.match(/^\*.+\*$/))
+      .map((m) => m.substr(1, m.length - 2)),
+  endWith: (list) =>
+    list.filter((m) => m.match(/^\*[^\*]+$/)).map((m) => m.substr(1)),
+  startWith: (list) =>
+    list
+      .filter((m) => m.match(/^[^\*\!]+\*$/))
+      .map((m) => m.substr(0, m.length - 1)),
+  notExact: (list) =>
+    list.filter((m) => m.match(/^\![^\*].*$/)).map((m) => m.substr(1)),
+  notContain: (list) =>
+    list
+      .filter((m) => m.match(/^\!\*.+\*$/))
+      .map((m) => m.substr(2, m.length - 3)),
+  notEndWith: (list) =>
+    list.filter((m) => m.match(/^\!\*[^\*]+$/)).map((m) => m.substr(2)),
+  notStartWith: (list) =>
+    list
+      .filter((m) => m.match(/^\![^\*]+\*$/))
+      .map((m) => m.substr(1, m.length - 2)),
+};
 
-var postcss = require('postcss');
-var objectAssign = require('object-assign');
-var { createPropListMatcher } = require('./src/prop-list-matcher');
-var { getUnitRegexp } = require('./src/pixel-unit-regexp');
+function createPropListMatcher(propList) {
+  var hasWild = propList.indexOf("*") > -1;
+  var matchAll = hasWild && propList.length === 1;
+  var lists = {
+    exact: filterPropList.exact(propList),
+    contain: filterPropList.contain(propList),
+    startWith: filterPropList.startWith(propList),
+    endWith: filterPropList.endWith(propList),
+    notExact: filterPropList.notExact(propList),
+    notContain: filterPropList.notContain(propList),
+    notStartWith: filterPropList.notStartWith(propList),
+    notEndWith: filterPropList.notEndWith(propList),
+  };
+  return function (prop) {
+    if (matchAll) return true;
+    return (
+      (hasWild ||
+        lists.exact.indexOf(prop) > -1 ||
+        lists.contain.some((m) => prop.indexOf(m) > -1) ||
+        lists.startWith.some((m) => prop.indexOf(m) === 0) ||
+        lists.endWith.some(
+          (m) => prop.indexOf(m) === prop.length - m.length
+        )) &&
+      !(
+        lists.notExact.indexOf(prop) > -1 ||
+        lists.notContain.some((m) => prop.indexOf(m) > -1) ||
+        lists.notStartWith.some((m) => prop.indexOf(m) === 0) ||
+        lists.notEndWith.some((m) => prop.indexOf(m) === prop.length - m.length)
+      )
+    );
+  };
+}
 
-var defaults = {
-  unitToConvert: 'px',
+const PLUGIN_DEFAULT_OPTIONS = {
+  unitToConvert: "px",
   viewportWidth: 320,
   viewportHeight: 568, // not now used; TODO: need for different units and math for different properties
   unitPrecision: 5,
-  viewportUnit: 'vw',
-  fontViewportUnit: 'vw',  // vmin is more suitable.
+  viewportUnit: "vw",
+  fontViewportUnit: "vw", // vmin is more suitable.
   selectorBlackList: [],
-  propList: ['*'],
+  propList: ["*"],
   minPixelValue: 1,
   mediaQuery: false,
   replace: true,
   landscape: false,
-  landscapeUnit: 'vw',
-  landscapeWidth: 568
+  landscapeUnit: "vw",
+  landscapeWidth: 568,
 };
 
-var ignoreNextComment = 'px-to-viewport-ignore-next';
-var ignorePrevComment = 'px-to-viewport-ignore';
+function toString(target) {
+  return Object.prototype.toString.call(target);
+}
 
-module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
-  var opts = objectAssign({}, defaults, options);
-
-  checkRegExpOrArray(opts, 'exclude');
-  checkRegExpOrArray(opts, 'include');
-
+function postcssPX2VW(options) {
+  var opts = Object.assign({}, PLUGIN_DEFAULT_OPTIONS, options);
   var pxRegex = getUnitRegexp(opts.unitToConvert);
   var satisfyPropList = createPropListMatcher(opts.propList);
   var landscapeRules = [];
 
-  return function (css, result) {
-    css.walkRules(function (rule) {
-      // Add exclude option to ignore some files like 'node_modules'
+  function exclude(opts, file) {
+    if (opts.exclude && file) {
+      if (Array.isArray(opts.exclude))
+        return opts.exclude.some((rule) => isExclude(rule, file));
+      return isExclude(opts.exclude, file);
+    }
+    return false;
+  }
+
+  function Once(root, { AtRule }) {
+    root.walkRules(function (rule) {
       var file = rule.source && rule.source.input.file;
 
-      if (opts.include && file) {
-        if (Object.prototype.toString.call(opts.include) === '[object RegExp]') {
-          if (!opts.include.test(file)) return;
-        } else if (Object.prototype.toString.call(opts.include) === '[object Array]') {
-          var flag = false;
-          for (var i = 0; i < opts.include.length; i++) {
-            if (opts.include[i].test(file)) {
-              flag = true;
-              break;
-            }
-          }
-          if (!flag) return;
-        }
-      }
-
-      if (opts.exclude && file) {
-        if (Object.prototype.toString.call(opts.exclude) === '[object RegExp]') {
-          if (opts.exclude.test(file)) return;
-        } else if (Object.prototype.toString.call(opts.exclude) === '[object Array]') {
-          for (var i = 0; i < opts.exclude.length; i++) {
-            if (opts.exclude[i].test(file)) return;
-          }
-        }
-      }
-
+      if (exclude(opts, file)) return;
       if (blacklistedSelector(opts.selectorBlackList, rule.selector)) return;
 
       if (opts.landscape && !rule.parent.params) {
         var landscapeRule = rule.clone().removeAll();
 
-        rule.walkDecls(function(decl) {
+        rule.walkDecls(function (decl) {
           if (decl.value.indexOf(opts.unitToConvert) === -1) return;
           if (!satisfyPropList(decl.prop)) return;
 
-          landscapeRule.append(decl.clone({
-            value: decl.value.replace(pxRegex, createPxReplace(opts, opts.landscapeUnit, opts.landscapeWidth))
-          }));
+          landscapeRule.append(
+            decl.clone({
+              value: decl.value.replace(
+                pxRegex,
+                createPxReplace(opts, opts.landscapeUnit, opts.landscapeWidth)
+              ),
+            })
+          );
         });
 
         if (landscapeRule.nodes.length > 0) {
@@ -86,34 +135,15 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
 
       if (!validateParams(rule.parent.params, opts.mediaQuery)) return;
 
-      rule.walkDecls(function(decl, i) {
+      rule.walkDecls(function (decl, i) {
         if (decl.value.indexOf(opts.unitToConvert) === -1) return;
         if (!satisfyPropList(decl.prop)) return;
-
-        var prev = decl.prev();
-        // prev declaration is ignore conversion comment at same line
-        if (prev && prev.type === 'comment' && prev.text === ignoreNextComment) {
-          // remove comment
-          prev.remove();
-          return;
-        }
-        var next = decl.next();
-        // next declaration is ignore conversion comment at same line
-        if (next && next.type === 'comment' && next.text === ignorePrevComment) {
-          if (/\n/.test(next.raws.before)) {
-            result.warn('Unexpected comment /* ' + ignorePrevComment + ' */ must be after declaration at same line.', { node: next });
-          } else {
-            // remove comment
-            next.remove();
-            return;
-          }
-        }
 
         var unit;
         var size;
         var params = rule.parent.params;
 
-        if (opts.landscape && params && params.indexOf('landscape') !== -1) {
+        if (opts.landscape && params && params.indexOf("landscape") !== -1) {
           unit = opts.landscapeUnit;
           size = opts.landscapeWidth;
         } else {
@@ -121,7 +151,10 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
           size = opts.viewportWidth;
         }
 
-        var value = decl.value.replace(pxRegex, createPxReplace(opts, unit, size));
+        var value = decl.value.replace(
+          pxRegex,
+          createPxReplace(opts, unit, size)
+        );
 
         if (declarationExists(decl.parent, decl.prop, value)) return;
 
@@ -134,18 +167,26 @@ module.exports = postcss.plugin('postcss-px-to-viewport', function (options) {
     });
 
     if (landscapeRules.length > 0) {
-      var landscapeRoot = new postcss.atRule({ params: '(orientation: landscape)', name: 'media' });
-
-      landscapeRules.forEach(function(rule) {
-        landscapeRoot.append(rule);
+      var media = new AtRule({
+        params: "(orientation: landscape)",
+        name: "media",
       });
-      css.append(landscapeRoot);
+
+      landscapeRules.forEach((rule) => media.append(rule));
+      root.append(media);
     }
+  }
+
+  return {
+    postcssPlugin: "px2vw",
+    Once,
   };
-});
+}
 
 function getUnit(prop, opts) {
-  return prop.indexOf('font') === -1 ? opts.viewportUnit : opts.fontViewportUnit;
+  return prop.indexOf("font") === -1
+    ? opts.viewportUnit
+    : opts.fontViewportUnit;
 }
 
 function createPxReplace(opts, viewportUnit, viewportSize) {
@@ -153,52 +194,41 @@ function createPxReplace(opts, viewportUnit, viewportSize) {
     if (!$1) return m;
     var pixels = parseFloat($1);
     if (pixels <= opts.minPixelValue) return m;
-    var parsedVal = toFixed((pixels / viewportSize * 100), opts.unitPrecision);
-    return parsedVal === 0 ? '0' : parsedVal + viewportUnit;
+    var parsedVal = toFixed((pixels / viewportSize) * 100, opts.unitPrecision);
+    return parsedVal === 0 ? "0" : parsedVal + viewportUnit;
   };
-}
-
-function error(decl, message) {
-  throw decl.error(message, { plugin: 'postcss-px-to-viewport' });
-}
-
-function checkRegExpOrArray(options, optionName) {
-  var option = options[optionName];
-  if (!option) return;
-  if (Object.prototype.toString.call(option) === '[object RegExp]') return;
-  if (Object.prototype.toString.call(option) === '[object Array]') {
-    var bad = false;
-    for (var i = 0; i < option.length; i++) {
-      if (Object.prototype.toString.call(option[i]) !== '[object RegExp]') {
-        bad = true;
-        break;
-      }
-    }
-    if (!bad) return;
-  }
-  throw new Error('options.' + optionName + ' should be RegExp or Array of RegExp.');
 }
 
 function toFixed(number, precision) {
   var multiplier = Math.pow(10, precision + 1),
     wholeNumber = Math.floor(number * multiplier);
-  return Math.round(wholeNumber / 10) * 10 / multiplier;
+  return (Math.round(wholeNumber / 10) * 10) / multiplier;
 }
 
 function blacklistedSelector(blacklist, selector) {
-  if (typeof selector !== 'string') return;
+  if (typeof selector !== "string") return;
   return blacklist.some(function (regex) {
-    if (typeof regex === 'string') return selector.indexOf(regex) !== -1;
+    if (typeof regex === "string") return selector.indexOf(regex) !== -1;
     return selector.match(regex);
   });
 }
 
+function isExclude(reg, file) {
+  if (!isRegExp(reg)) throw new Error("options.exclude should be RegExp.");
+  return file.match(reg) !== null;
+}
 function declarationExists(decls, prop, value) {
   return decls.some(function (decl) {
-      return (decl.prop === prop && decl.value === value);
+    return decl.prop === prop && decl.value === value;
   });
 }
 
 function validateParams(params, mediaQuery) {
   return !params || (params && mediaQuery);
 }
+
+function isRegExp(target) {
+  return toString(target) == "[object RegExp]";
+}
+postcssPX2VW.postcss = true;
+export default postcssPX2VW;
